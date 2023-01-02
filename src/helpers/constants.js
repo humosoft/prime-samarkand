c28660faaf6b8cbbb01382b497ad7f388a6674a6
vecume <shahzod.bek200109@gmail.com>
2023-01-03 02:48:44 +0500

    Change server url

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = "http://localhost:1337"
+export const SERVER_URL = "https://api.prime-samarkand.com"
 export const API_URL = `${SERVER_URL}/api`;
 export const CAPTCHA_KEY = "6Lce8MYjAAAAAJnBpXyJHNPkKQ4fmh8NG9C_a90q"
